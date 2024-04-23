a7dfa77bc2ded2fecc6c6f745c2621c313b8aae0
Max Rakitin <mrakitin@bnl.gov>
2024-04-23 15:40:15 -0700

    Fix precision for the beamstop

--- startup/21-detectors.py
+++ startup/21-detectors.py
@@ -1,3 +1,12 @@
 print(f"Loading {__file__}")
+from ophyd import Device, EpicsSignal, Component as Cpt
 
-diode_sample = ophyd.EpicsSignal('bl201-beamstop:current', name='diode_sample')
+class BeamStopCurrent(Device):
+    prec = Cpt(EpicsSignal, ".PREC")
+    current = Cpt(EpicsSignal, "")
+
+
+beamstop_current_device = BeamStopCurrent("bl201-beamstop:current", name="beamstop_current")
+beamstop_current_device.prec.put(3)  # This is needed for LiveTable (via BestEffortCallback) to print the values with proper number of decimal values
+beamstop_current = beamstop_current_device.current
+beamstop_current.kind = "hinted"
